325e6f9990fd94be62bc1f269426ffaa1d003fb9
smallBrilliant <wh_199610@163.com>
2022-10-09 16:21:31 +0800

    add goreleaser

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"code": 520, "msg": "谭谭我好喜欢你吖"})
+		c.JSON(200, gin.H{"code": 520, "msg": "!!!!!谭谭我好喜欢你吖!!!!"})
 	})
 	r.Run(":8080")
 }
